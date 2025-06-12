8856a7c57dab42a58b6c3124e90495bda4de2eb6
Raccom <tyuf64@163.com>
2025-06-12 11:37:45 +0800

    doc: 修改部分文档

--- src/scripts/notification.ts
+++ src/scripts/notification.ts
@@ -32,7 +32,7 @@ export const showNotification = (data: NotificationData) => {
     // eslint-disable-next-line
     new Notification(data.title, {
       body: data.body,
-      icon: 'https://cdn.tiltysola.com/assets/tilty/images/tilty_logo_round.png',
+      icon: '/images/tilty_logo_round_white.png',
     });
   } else {
     toast.info(`${data.title}: ${data.body}`);
